ea5a312e76bfd210fa58f083dab0d3af30da25da
gautamdhw <gautamdhawan910@gmail.com>
2024-07-24 00:31:16 +0530

    code

---
+++ insertionsort.cpp
@@ -0,0 +1,16 @@
+void insertionSort(int n, vector<int> &arr){
+    // Write your code here.
+    for(int i=1;i<n;i++)
+    {
+        int j=i-1;
+        int key=arr[i];
+
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+
+        arr[j+1]=key;
+    }
+}
